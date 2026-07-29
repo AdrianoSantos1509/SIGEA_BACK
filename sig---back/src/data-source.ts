95e97b9504d0355f97665029e7c503fd1ef1f0ad
RafaelLimaTarnac <rtarnac@gmail.com>
2026-07-29 20:36:02 -0300

    arrumei_dotenv

--- sig---back/src/data-source.ts
+++ sig---back/src/data-source.ts
@@ -8,15 +8,15 @@ import { Occupancy } from "./entities/occupancy"
 import { Coordinator } from "./entities/coordinator"
 import { Building } from "./entities/building"
 
-//require("dotenv").config();
+require("dotenv").config();
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
+    host: process.env.DB_HOST,
     port: 3306,
-    username: "root",
-    password: "senac",
-    database: "senac_type",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     synchronize: true,
     logging: false,
     entities: [User, Classroom, Course, Occupancy, Coordinator, Building],
