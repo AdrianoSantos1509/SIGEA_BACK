32f76044120a14f7181739d0e4e9d70a990c5528
AdrianoSantos1509 <adriianosantos85@gmail.com>
2026-08-06 19:57:11 -0300

    Ajustando arquivos

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
 import { UserRoutes } from "./routes/user";
 
 import { AppDataSource } from "./data-source";
-import { encryptData } from "./middleware/bcrypt.middleware";
 const express = require("express");
 const app = express();
 
 app.use(express.json());
 app.use("/users", UserRoutes);
-
-import { generateToken, verifyToken } from "./middleware/token.middleware";
 
 AppDataSource.initialize().then(async () => {
 	console.log("database connected");
